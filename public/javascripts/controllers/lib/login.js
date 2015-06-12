0a096c2b02add27e9b6439683a4ad681347a85c1
Lanz <zihao.lan@dianrong.com>
2015-06-12 18:30:09 +0800

    void(0)

--- public/javascripts/controllers/lib/login.js
+++ public/javascripts/controllers/lib/login.js
@@ -2,64 +2,102 @@
  * Created by lanz on 2015/6/11.
  */
 var angular = require('angular');
-var ApiService = require('../../services/lib/api-services');
 
-var loginModule = angular.module('loginModule', [
-	ApiService.name
-]);
+var loginModule = angular.module('loginModule', []);
 
 loginModule.controller('loginCtrl', [
 	'$q',
 	'$rootScope',
 	'$scope',
 	'$http',
-	'ApiService',
-	function($q, $rootScope, $scope, $http, ApiService) {
-	//$http.defaults.headers.post = {
-	//	'Access-Control-Allow-Origin': 'localhost',
-	//	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-	//	'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
-	//};
-	//$http.defaults.useXDomain = true;
+	'$location',
+	function ($q, $rootScope, $scope, $http, $location) {
 
-	$http.defaults.headers.get = {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	};
+		var CLIENT_ID = '416d4cd8a701b785040bc29cdd067ecd';
+		var CLIENT_KEY = '99bae2161aa664595666a86c12545fb8e8ffe9f6';
 
-	$http.defaults.headers.post = {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	};
+		$http.defaults.headers.get = {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		};
 
-	delete $http.defaults.headers.common['X-Requested-With'];
+		$http.defaults.headers.post = {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		};
 
-	$scope.submit = function() {
+		$scope.$watch('$location.path()', function () {
+			$scope.oAuthCb();
+		});
 
-		ApiService.login($scope.email, $scope.pd)
-			.then(function (response) {
-				window.location.reload();
-			}, function (response) {
-				console.log(response);
-			});
+		$scope.code = '';
 
-		// WARN: This will cause CORS
-		//$http.post("https://coding.net/api/account/login", {
-		//	email: $scope.email,
-		//	password: $scope.pd
-		//}).success(function(response) {
-		//	console.log(response);
-		//}).error(function(e) {
-		//	console.log(e);
-		//});
+		$scope.getURLParam = function (params) {
+			var arr = decodeURI(location.search.substring(1)).split('&');
+			for(var i = 0, len = arr.length; i < len; i++){
+				if(arr[i].substring(0, arr[i].indexOf('=')) == params) {
+					return arr[i].substring(arr[i].indexOf('=')+1);
+				}
+			}
+		};
 
-		// TEST
-		console.log($http.defaults);
-	};
+		$scope.oAuthCb = function () {
+			$scope.code = $scope.getURLParam('code');
+			if ($scope.code) {
+				console.log($scope.code);
+				window.location.href = '/callback/' + CLIENT_ID + '/' + CLIENT_KEY + '/' + $scope.code;
+				//var url = 'https://coding.net/api/oauth/access_token?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_KEY + '&grant_type=authorization_code&code=' + $scope.code;
+				/*var deferred = $q.defer(); // Create Promise
+				var url = 'https://coding.net/api/oauth/access_token';
+				console.log(url);
+				deferred.promise.xhr = $.ajax(url, {
+					'dataType': 'jsonp',
+					'traditional': true,
+					'data': {
+						client_id: CLIENT_ID,
+						client_secret: CLIENT_KEY,
+						grant_type: 'authorization_code',
+						code: $scope.code
+					},
+					'timeout': 120000,
+					'success': function (response, status, xhr) {
+						try {
+							console.log(xhr);
+							if (response) {
+								console.log(response);
+								deferred.resolve(response, status, xhr);
+							} else {
+								console.log('success else condition');
+								deferred.reject(response, status, xhr || 'unknown');
+							}
+						} catch (e) {
+							console.log(e);
+						}
 
-	$scope.checkLogin = function () {
-		console.log('init check');
-	};
 
-	$scope.checkLogin();
-}]);
+						$rootScope.$apply();
+					},
+					'error': function (xhr, status, error) {
+						var response = $.parseJSON(xhr.responseText);
+						console.log(response);
+						deferred.reject(response, status, xhr, error);
+
+						$rootScope.$apply();
+					}
+				});*/
+			} else {
+				console.log('Cannot get return code');
+			}
+		};
+
+		$scope.oAuth = function () {
+			window.location.href = 'https://coding.net/oauth_authorize.html?client_id=' + CLIENT_ID + '&redirect_uri=http://localhost:3001/index&response_type=code&scope=all';
+		};
+
+		$scope.checkLogin = function () {
+			console.log('init check');
+		};
+
+		$scope.checkLogin();
+	}
+]);
 
 module.exports = loginModule;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var jade = require('jade');
 var router = express.Router();
 
@@ -8,6 +9,29 @@ router.get('/index', function(req, res, next){
 	} else {
 		next();
 	}
+});
+
+router.get('/callback/:clientId/:clientKey/:code', function (req, res, next) {
+	var options = {
+		host: 'www.coding.net',
+		port: 80,
+		path: '/api/oauth/access_token?client_id=' + req.params.clientId + '&client_secret=' + req.params.clientKey + '&grant_type=authorization_code&code=' + req.params.code
+	};
+
+	var req = http.request(options, function(res) {
+		console.log(res);
+		res.on('data', function (chunk) {
+			console.log('BODY: ' + chunk);
+		});
+	});
+
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+// write data to request body
+	req.write('data\n');
+	req.end();
 });
 
 module.exports = router;
